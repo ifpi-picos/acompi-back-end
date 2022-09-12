42d4f42a18a31268d4292d9ba99c3bbc12aa5f90
Gabriel <victorgrinfindor@gmail.com>
2022-09-12 16:35:56 -0300

    Adicionando rotas do adm

---
+++ src/rotas/administrador.ts
@@ -0,0 +1,14 @@
+import { Router, Request, Response } from 'express';
+
+const rotas = Router();
+
+rotas.get('/', (req: Request, res: Response) => {
+    res.end('Turmas');
+});
+
+
+rotas.delete('/', (req: Request, res: Response) => {
+    res.end('Deletou e-mail');
+});
+
+export default rotas;
--- src/rotas/index.ts
+++ src/rotas/index.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from 'express';
 import rotasAluno from './alunos';
 import rotasProfessor from './professor';
+import rotasAdministrador from './administrador';
 
 const rotas = Router();
 
@@ -10,5 +11,6 @@ rotas.get('/', (req: Request, res: Response) => {
 
 rotas.use('/alunos', rotasAluno);
 rotas.use('/professor', rotasProfessor);
+rotas.use('/administrador', rotasAdministrador);
 
 export default rotas;
