a6c36f2e3db3929c6bc2b7b70766e26d7b31abe0
Allan Barbosa <allanb3779@gmail.com>
2022-09-15 16:53:32 -0300

    alterando rota alunos

--- src/rotas/alunos.ts
+++ src/rotas/alunos.ts
@@ -6,7 +6,7 @@ rotas.get('/', (req: Request, res: Response) => {
   res.json({ aluno: 'lista de alunos' });
 });
 
-rotas.get('/', (req: Request, res: Response) => {
+rotas.post('/', (req: Request, res: Response) => {
   res.json({ aluno: 'aluno adicionado' });
 });
 
