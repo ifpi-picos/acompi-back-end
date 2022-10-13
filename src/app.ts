941a222ed7dce991e851aad257f13d2e76ea5d58
Allan Barbosa <allanb3779@gmail.com>
2022-10-13 00:32:28 -0300

    adicionando jest ao projeto

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import express from 'express';
-import rotas from './rotas'
+import rotas from './rotas';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/', rotas)
+app.use('/', rotas);
 
-app.listen(3000, () =>
-console.log('Servidor funcionando!'));
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import app from './app';
+
+const port = process.env.PORT || 3000;
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`Servidor funcionando ${port}!`));
