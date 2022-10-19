f103610b6d12d7d1934093ae9be2fd11f864dbaa
IagoBrenoAD <iagobrenoaraujodamasceno@gmail.com>
2022-10-19 12:14:18 -0300

    Adicionando rotas para criar turma do professor.

--- src/rotas/professor.ts
+++ src/rotas/professor.ts
@@ -1,17 +1,42 @@
 import { Router, Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 const rotas = Router();
 
-rotas.get('/', (req: Request, res: Response) => {
-    res.end('turmas');
+rotas.get('/visualizarturmas', async (req: Request, res: Response) => {
+    const visualizarturmas = await prisma.criarTurma.findMany({});
+    res.status(200).json(visualizarturmas);
 });
 
-rotas.post('/', (req: Request, res: Response) => { 
-    res.end('adicionou turma');
+rotas.post('/criar_turma', async (req: Request, res: Response) => {
+  const { id_turma, email_professor, id_lab, data_turma, horario_inicio, horario_fim, curso } = req.body;
+  try{
+    const criarTurma = await prisma.criarTurma.create({
+      data: {
+        id_turma,
+        email_professor,
+        id_lab,
+        data_turma,
+        horario_inicio,
+        horario_fim,
+        curso,
+      },
+    });
+    res.status(201).json(criarTurma);
+  } catch (erro){
+    res.status(400).send(erro);
+  }
 });
 
-rotas.delete('/', (req: Request, res: Response) => {
-    res.end('deletou');
+rotas.delete('/cancelar', async (req: Request, res: Response) => {
+  const {email_professor} = req.body;
+  const delete_criarTurma = await prisma.criarTurma.delete({
+      where: {
+          email_professor: email_professor,
+      },
+  })
+    res.status(200).json(delete_criarTurma)
 });
 
 export default rotas;
