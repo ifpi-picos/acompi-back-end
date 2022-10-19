fb60acf94632dc5e5a4016c12a6821090dc58974
IagoBrenoAD <iagobrenoaraujodamasceno@gmail.com>
2022-10-19 16:57:15 -0300

    Criando tests da rota professor

--- src/rotas/professor.ts
+++ src/rotas/professor.ts
@@ -5,14 +5,14 @@ const prisma = new PrismaClient();
 const rotas = Router();
 
 rotas.get('/visualizarturmas', async (req: Request, res: Response) => {
-    const visualizarturmas = await prisma.criarTurma.findMany({});
+    const visualizarturmas = await prisma.criarturma.findMany({});
     res.status(200).json(visualizarturmas);
 });
 
 rotas.post('/criar_turma', async (req: Request, res: Response) => {
   const { id_turma, email_professor, id_lab, data_turma, horario_inicio, horario_fim, curso } = req.body;
   try{
-    const criarTurma = await prisma.criarTurma.create({
+    const criarTurma = await prisma.criarturma.create({
       data: {
         id_turma,
         email_professor,
@@ -31,7 +31,7 @@ rotas.post('/criar_turma', async (req: Request, res: Response) => {
 
 rotas.delete('/cancelar', async (req: Request, res: Response) => {
   const {email_professor} = req.body;
-  const delete_criarTurma = await prisma.criarTurma.delete({
+  const delete_criarTurma = await prisma.criarturma.delete({
       where: {
           email_professor: email_professor,
       },
