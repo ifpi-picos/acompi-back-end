c2954191107a701e0dfc0f51eaf1467262b1e910
Allan Barbosa <allanb3779@gmail.com>
2022-10-21 17:20:38 -0300

    alterando rota reservas

--- src/rotas/index.ts
+++ src/rotas/index.ts
@@ -1,8 +1,6 @@
 import { Router, Request, Response } from 'express';
-import rotasAluno from './alunos';
-// import rotasProfessor from './professor';
+import rotasReservas from './reservas';
 import rotasAdministrador from './administrador';
-// import rotasAutenticacao from './autenticacao';
 import rotasTurmas from './turmas';
 
 const rotas = Router();
@@ -12,10 +10,8 @@ rotas.get('/', (req: Request, res: Response) => {
   res.send('https://ifpi-picos.github.io/acompi-front-end/');
 });
 
-rotas.use('/aluno', rotasAluno);
-// rotas.use('/professor', rotasProfessor);
+rotas.use('/reservas', rotasReservas);
 rotas.use('/administrador', rotasAdministrador);
-// rotas.use('/autenticacao', rotasAutenticacao);
 rotas.use('/turmas', rotasTurmas);
 
 export default rotas;
---
+++ testes/integração/reservas.test.ts
@@ -0,0 +1,48 @@
+import 'jest';
+import request from 'supertest';
+import app from '../../src/app';
+import prisma from '../helpers/index';
+
+beforeAll(async () => {
+  const removeReservas= prisma.reserva.deleteMany();
+  await prisma.$transaction([removeReservas]);
+});
+
+afterAll(async () => {
+  const removeReservas = prisma.reserva.deleteMany();
+  await prisma.$transaction([removeReservas]);
+  await prisma.$disconnect();
+});
+
+
+describe('Testando reservas dos alunos', () => {
+  // test('testando o post da reserva', async () => {
+  //   const novaReserva = {
+  //     id_aluno: 1,
+  //     id_turma: 1,
+  //   };
+  //   const response = await request(app).post('/reservas').send(novaReserva);
+  //   expect(response.statusCode).toBe(201);
+  //   });
+
+    test('testando o get das reservas', async () => {
+      const response = await request(app).get('/reservas');
+      const reservas = response.body;
+      expect(response.statusCode).toBe(200);
+      expect(reservas.length).toBe(1);
+    });
+
+    // test('testando o delete das reservas', async () => {
+    //   const novaReserva = {
+    //   id_aluno: 2,
+    //   id_turma: 1,
+    //   };
+
+    //   const cancelaReserva = {
+    //   id_reserva: 1,
+    //   };
+    //   let response = await request(app).post('/reservas').send(novaReserva);
+    //   response = await request(app).delete('/reservas').send(cancelaReserva);
+    //   expect(response.statusCode).toBe(200);
+    // });
+});
