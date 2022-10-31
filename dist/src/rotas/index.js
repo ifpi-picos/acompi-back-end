02c6a791d33a183ae2f16873dbc57d819fad58b1
Allan Barbosa <allanb3779@gmail.com>
2022-10-31 16:54:37 -0300

    alterações na tabela reserva

--- dist/src/rotas/index.js
+++ dist/src/rotas/index.js
@@ -5,15 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const reservas_1 = __importDefault(require("./reservas"));
-const administrador_1 = __importDefault(require("./administrador"));
+const proibe_lab_1 = __importDefault(require("./proibe-lab"));
 const turmas_1 = __importDefault(require("./turmas"));
+// import rotasPbloqueia from './pbloqueia-laboratorio';
 const rotas = (0, express_1.Router)();
 rotas.get('/', (req, res) => {
     // levando usuário para a home
     res.send('https://ifpi-picos.github.io/acompi-front-end/');
 });
 rotas.use('/reservas', reservas_1.default);
-rotas.use('/administrador', administrador_1.default);
+rotas.use('/administrador', proibe_lab_1.default);
 rotas.use('/turmas', turmas_1.default);
+// rotas.use('/pbloqueia-laboratorio', rotasPbloqueia);
 exports.default = rotas;
 //# sourceMappingURL=index.js.map
