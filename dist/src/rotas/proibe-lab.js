57f25733cd0ac683e0fba0d5e5c6a875aa7aa31d
Allan Barbosa <allanb3779@gmail.com>
2022-10-31 17:43:10 -0300

    alterando o schema do prisma

--- dist/src/rotas/proibe-lab.js
+++ dist/src/rotas/proibe-lab.js
@@ -4,21 +4,21 @@ const express_1 = require("express");
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
 const rotas = (0, express_1.Router)();
-rotas.post('/laboratorio', async (req, res) => {
-    const { /*email_adm, id_lab,*/ data_inicio, data_fim } = req.body;
-    try {
-        const proibicao = await prisma.proibeLaboratorio.create({
-            data: {
-                data_inicio: data_inicio,
-                data_fim: data_fim
-            },
-        });
-        res.status(201).json(proibicao);
-    }
-    catch (erro) {
-        res.status(400).send(erro);
-    }
-});
+// erro pra dar deploy no servidor
+// rotas.post('/laboratorio', async (req: Request, res: Response) => {
+//     const { /*email_adm, id_lab,*/data_inicio, data_fim } = req.body;
+//     try {
+//       const proibicao = await prisma.proibeLaboratorio.create({
+//         data: {
+//           data_inicio: data_inicio,
+//           data_fim: data_fim
+//         }as IRequest,
+//       });
+//       res.status(201).json(proibicao);
+//     } catch (erro) {
+//       res.status(400).send(erro);
+//     }
+//   });
 rotas.delete('/', (req, res) => {
     res.end('Deletou e-mail');
 });
--- src/rotas/proibe-lab.ts
+++ src/rotas/proibe-lab.ts
@@ -10,20 +10,21 @@ interface IRequest {
 const prisma = new PrismaClient();
 const rotas = Router();
 
-rotas.post('/laboratorio', async (req: Request, res: Response) => {
-    const { /*email_adm, id_lab,*/data_inicio, data_fim } = req.body;
-    try {
-      const proibicao = await prisma.proibeLaboratorio.create({
-        data: {
-          data_inicio: data_inicio,
-          data_fim: data_fim
-        }as IRequest,
-      });
-      res.status(201).json(proibicao);
-    } catch (erro) {
-      res.status(400).send(erro);
-    }
-  });
+// erro pra dar deploy no servidor
+// rotas.post('/laboratorio', async (req: Request, res: Response) => {
+//     const { /*email_adm, id_lab,*/data_inicio, data_fim } = req.body;
+//     try {
+//       const proibicao = await prisma.proibeLaboratorio.create({
+//         data: {
+//           data_inicio: data_inicio,
+//           data_fim: data_fim
+//         }as IRequest,
+//       });
+//       res.status(201).json(proibicao);
+//     } catch (erro) {
+//       res.status(400).send(erro);
+//     }
+//   });
 
 rotas.delete('/', (req: Request, res: Response) => {
     res.end('Deletou e-mail');
