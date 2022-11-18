49f6670b86c19c7e1220bb4610c8966ba6623719
edivan <wesleyvc2110@gmail.com>
2022-11-17 21:37:20 -0300

    Verificando erros

--- dist/src/server.js
+++ dist/src/server.js
@@ -4,8 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-const port = process.env.PORT || 3000;
-// eslint-disable-next-line no-console
-app_1.default.listen(port, () => console.log(`Servidor funcionando ${port}!`));
+// const port = process.env.PORT || 3000;
+// // eslint-disable-next-line no-console
+// app.listen(port, () => console.log(`Servidor funcionando ${port}!`));
 exports.default = app_1.default;
 //# sourceMappingURL=server.js.map
