623ca33f4d953f7fc61064783ce5ee0e42d9ea97
Allan Barbosa <allanb3779@gmail.com>
2023-10-30 19:36:41 -0300

    atualizando CORS

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(cors({
-    origin: process.env.FRONTEND || "*", // url do front
+    origin: process.env.FRONTEND, // url do front
     credentials: true,
     methods: 'GET, PUT, POST, OPTIONS, DELETE, PATCH',
 }));
